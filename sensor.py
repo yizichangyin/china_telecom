eddf8a521318e39d93dc76179c0d74c3491a2e3d
小叶 <45478919+yizichangyin@users.noreply.github.com>
2025-03-13 04:57:16 +0800

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,1 +1,11 @@
 
+from homeassistant.helpers.entity import Entity
+
+class MySensor(Entity):
+    @property
+    def name(self):
+        return "My Custom Sensor"
+
+    @property
+    def state(self):
+        return "Hello World"
